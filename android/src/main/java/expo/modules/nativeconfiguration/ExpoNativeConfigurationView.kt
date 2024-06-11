34377415d09d7fb1a9fd09477aa23e5c08f4c65a
onat varis <onatvaris@gmail.com>
2024-06-12 00:15:50 +0300

    chore: Remove unused ExpoNativeConfiguration files

--- android/src/main/java/expo/modules/nativeconfiguration/ExpoNativeConfigurationView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.nativeconfiguration
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoNativeConfigurationView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
